211dd42ea04ee3de94d7313f6aa65d2cf5d05873
muhammet şahmutoğlu <genc_muhammet_1967@hotmail.com>
2022-12-02 21:42:44 +0300

    Add project files.

---
+++ DataTableServerSide.UI/Controllers/EmployeesController.cs
@@ -0,0 +1,67 @@
+﻿using DataTableServerSide.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataTableServerSide.UI.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: Employees
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData(JqueryDatatableParam param)
+        {
+            Employee emp=new Employee();
+
+            var employees = emp.GetEmployees(); //This method is returning the IEnumerable employee from database
+
+            if (!string.IsNullOrEmpty(param.sSearch))
+            {
+                employees = employees.Where(x => x.Name.ToLower().Contains(param.sSearch.ToLower())
+                                              || x.Position.ToLower().Contains(param.sSearch.ToLower())
+                                              || x.Location.ToLower().Contains(param.sSearch.ToLower())
+                                              || x.Salary.ToString().Contains(param.sSearch.ToLower())
+                                              || x.Age.ToString().Contains(param.sSearch.ToLower())
+                                              || x.StartDate.ToString("dd'/'MM'/'yyyy").ToLower().Contains(param.sSearch.ToLower())).ToList();
+            }
+
+            var sortColumnIndex = Convert.ToInt32(HttpContext.Request.QueryString["iSortCol_0"]);
+            var sortDirection = HttpContext.Request.QueryString["sSortDir_0"];
+            if (sortColumnIndex == 3)
+            {
+                employees = sortDirection == "asc" ? employees.OrderBy(c => c.Age) : employees.OrderByDescending(c => c.Age);
+            }
+            else if (sortColumnIndex == 4)
+            {
+                employees = sortDirection == "asc" ? employees.OrderBy(c => c.StartDate) : employees.OrderByDescending(c => c.StartDate);
+            }
+            else if (sortColumnIndex == 5)
+            {
+                employees = sortDirection == "asc" ? employees.OrderBy(c => c.Salary) : employees.OrderByDescending(c => c.Salary);
+            }
+            else
+            {
+                Func<Employee, string> orderingFunction = e => sortColumnIndex == 0 ? e.Name : sortColumnIndex == 1 ? e.Position : e.Location;
+                employees = sortDirection == "asc" ? employees.OrderBy(orderingFunction) : employees.OrderByDescending(orderingFunction);
+            }
+
+            var displayResult = employees.Skip(param.iDisplayStart)
+               .Take(param.iDisplayLength).ToList();
+            var totalRecords = employees.Count();
+
+            return Json(new
+            {
+                param.sEcho,
+                iTotalRecords = totalRecords,
+                iTotalDisplayRecords = totalRecords,
+                aaData = displayResult
+            }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ DataTableServerSide.UI/Models/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataTableServerSide.UI.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string Location { get; set; }
+        public int Age { get; set; }
+        public DateTime StartDate { get; set; }
+        public string StartDateString { get; set; }
+        public int Salary { get; set; }
+
+        public  IEnumerable<Employee> GetEmployees() 
+        { 
+            var employees = new List<Employee>();
+            employees.Add(new Employee {Name="Muhammet",Position="Developer",Location="İstanbul",Age=31,StartDate=DateTime.Now.AddYears(-5),StartDateString= DateTime.Now.AddYears(-5).ToShortDateString(),Salary=6000 });
+            employees.Add(new Employee { Name = "Gökhan", Position = "Developer", Location = "İstanbul", Age = 31, StartDate = DateTime.Now.AddYears(-5), StartDateString = DateTime.Now.AddYears(-5).ToShortDateString(), Salary = 6000 });
+            employees.Add(new Employee { Name = "İrem", Position = "Developer", Location = "İstanbul", Age = 31, StartDate = DateTime.Now.AddYears(-5), StartDateString = DateTime.Now.AddYears(-5).ToShortDateString(), Salary = 6000 });
+            employees.Add(new Employee { Name = "Tuba", Position = "Developer", Location = "İstanbul", Age = 31, StartDate = DateTime.Now.AddYears(-5), StartDateString = DateTime.Now.AddYears(-5).ToShortDateString(), Salary = 6000 });
+            employees.Add(new Employee { Name = "Ali", Position = "Developer", Location = "İstanbul", Age = 31, StartDate = DateTime.Now.AddYears(-5), StartDateString = DateTime.Now.AddYears(-5).ToShortDateString(), Salary = 6000 });
+            employees.Add(new Employee { Name = "Veli", Position = "Developer", Location = "İstanbul", Age = 31, StartDate = DateTime.Now.AddYears(-5), StartDateString = DateTime.Now.AddYears(-5).ToShortDateString(), Salary = 6000 });
+            employees.Add(new Employee { Name = "Ahmet", Position = "Developer", Location = "İstanbul", Age = 31, StartDate = DateTime.Now.AddYears(-5), StartDateString = DateTime.Now.AddYears(-5).ToShortDateString(), Salary = 6000 });
+            employees.Add(new Employee { Name = "Mehmet", Position = "Developer", Location = "İstanbul", Age = 31, StartDate = DateTime.Now.AddYears(-5), StartDateString = DateTime.Now.AddYears(-5).ToShortDateString(), Salary = 6000 });
+            employees.Add(new Employee { Name = "Hakan", Position = "Developer", Location = "İstanbul", Age = 31, StartDate = DateTime.Now.AddYears(-5), StartDateString = DateTime.Now.AddYears(-5).ToShortDateString(), Salary = 6000 });
+            employees.Add(new Employee { Name = "Hüseyin", Position = "Developer", Location = "İstanbul", Age = 31, StartDate = DateTime.Now.AddYears(-5), StartDateString = DateTime.Now.AddYears(-5).ToShortDateString(), Salary = 6000 });
+            employees.Add(new Employee { Name = "Koray", Position = "Developer", Location = "İstanbul", Age = 31, StartDate = DateTime.Now.AddYears(-5), StartDateString = DateTime.Now.AddYears(-5).ToShortDateString(), Salary = 6000 });
+            employees.Add(new Employee { Name = "Anıl", Position = "Developer", Location = "İstanbul", Age = 31, StartDate = DateTime.Now.AddYears(-5), StartDateString = DateTime.Now.AddYears(-5).ToShortDateString(), Salary = 6000 });
+            employees.Add(new Employee { Name = "Gencay", Position = "Developer", Location = "İstanbul", Age = 31, StartDate = DateTime.Now.AddYears(-5), StartDateString = DateTime.Now.AddYears(-5).ToShortDateString(), Salary = 6000 });
+            employees.Add(new Employee { Name = "Mert", Position = "Developer", Location = "İstanbul", Age = 31, StartDate = DateTime.Now.AddYears(-5), StartDateString = DateTime.Now.AddYears(-5).ToShortDateString(), Salary = 6000 });
+            employees.Add(new Employee { Name = "Okan", Position = "Developer", Location = "İstanbul", Age = 31, StartDate = DateTime.Now.AddYears(-5), StartDateString = DateTime.Now.AddYears(-5).ToShortDateString(), Salary = 6000 });
+            employees.Add(new Employee { Name = "Senem", Position = "Developer", Location = "İstanbul", Age = 31, StartDate = DateTime.Now.AddYears(-5), StartDateString = DateTime.Now.AddYears(-5).ToShortDateString(), Salary = 6000 });
+            employees.Add(new Employee { Name = "Özge", Position = "Developer", Location = "İstanbul", Age = 31, StartDate = DateTime.Now.AddYears(-5), StartDateString = DateTime.Now.AddYears(-5).ToShortDateString(), Salary = 6000 });
+            employees.Add(new Employee { Name = "Öznur", Position = "Developer", Location = "İstanbul", Age = 31, StartDate = DateTime.Now.AddYears(-5), StartDateString = DateTime.Now.AddYears(-5).ToShortDateString(), Salary = 6000 });
+            employees.Add(new Employee { Name = "Rıdvan", Position = "Developer", Location = "İstanbul", Age = 31, StartDate = DateTime.Now.AddYears(-5), StartDateString = DateTime.Now.AddYears(-5).ToShortDateString(), Salary = 6000 });
+            employees.Add(new Employee { Name = "Merve", Position = "Developer", Location = "İstanbul", Age = 31, StartDate = DateTime.Now.AddYears(-5), StartDateString = DateTime.Now.AddYears(-5).ToShortDateString(), Salary = 6000 });
+            employees.Add(new Employee { Name = "Ayşe", Position = "Developer", Location = "İstanbul", Age = 31, StartDate = DateTime.Now.AddYears(-5), StartDateString = DateTime.Now.AddYears(-5).ToShortDateString(), Salary = 6000 });
+
+            return employees;
+        }
+
+    }
+}
